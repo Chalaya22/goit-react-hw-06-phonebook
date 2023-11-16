3ca8d77c50747733179f904adb651db098527397
Chalaya22 <chalayaolga22@gmail.com>
2023-11-16 22:41:42 +0200

    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,15 @@
+import { createFilter } from 'redux/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import css from './Filter.module.css';
-const Filter = ({ filter, onFilter }) => {
+
+const Filter = ({}) => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter.Filter);
+
+  const onFilter = event => {
+    dispatch(createFilter(event.target.value));
+  };
+
   return (
     <div className={css.filter}>
       <label htmlFor="filter">
